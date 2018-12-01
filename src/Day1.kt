911f5a5aaf488496fde8df95e55c1176837113c8
Rodolfo Navalon <navalonrodolfo@gmail.com>
2018-11-30 22:47:45 -0500

    first commit

---
+++ src/Day1.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main() {
+    val day1 = Day1()
+    day1.part1()
+}
+
+class Day1 {
+
+    fun part1() {
+        val data = ""
+        val scanner = Scanner(data)
+        while(scanner.hasNextLine()) {
+            val line = scanner.nextLine()
+        }
+    }
+}
